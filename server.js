cbc49ce6114d81539713ad514b2ca440b4d1d8f6
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-10 22:22:21 +0200

    add the main.hbs layout and remove useless HTML code from subpage view files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-app.engine('.hbs', hbs());
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,27 +16,27 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.render('index', { layout: false });
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout: false });
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { layout: false });
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info', { layout: false });
+  res.render('info',);
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', { layout: false });
+  res.render('history');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
