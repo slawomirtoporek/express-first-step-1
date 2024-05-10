7b83e8baae5948a8e8e4749dd6ecd03d3a9e8ec2
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-10 14:32:54 +0200

    add endpoints for style.css and test.png

--- server.js
+++ server.js
@@ -30,9 +30,18 @@ app.get('/history', (req, res) => {
   res.show('history.html');
 });
 
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
+});
+
+app.get('/test.png', (req, res) => {
+  res.sendFile(path.join(__dirname, '/test.png'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
+
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
